1092cfd93e454bc0afa25f88069e9e3a6cd17675
Ben <ben@temme-home.de>
2019-11-30 16:07:10 +0100

    test2

--- Testscript.py
+++ Testscript.py
@@ -9,12 +9,7 @@ NUMPIXEL_1 = 205
 NUMPIXEL_2 = 230
 
 pixel_1 = neopixel.NeoPixel(PIN_1, NUMPIXEL_1)
-pixel_2 = neopixel.NeoPixel(PIN_2, NUMPIXEL_2)
 
-for i in range(NUMPIXEL_1 + NUMPIXEL_2):
-    if(i < NUMPIXEL_1):
-        pixel_1[i] = (120,120,120)
-    else:
-        pixel_2[i - NUMPIXEL_1] = (120,120,120)
+for i in range(NUMPIXEL_1):
+    pixel_1[i] = (120,120,120)
     pixel_1.show()
-    pixel_2.show()
