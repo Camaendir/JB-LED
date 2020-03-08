675600bb50d74adda72c7c57e7aba2433d437bde
Camaendir <bentemme.katec@gmail.com>
2020-03-08 01:27:53 +0100

    First version Puslar

--- Musik.py
+++ Musik.py
@@ -9,68 +9,9 @@ from colorsys import hls_to_rgb
 import time
 from Lib.SubEngine import *
 from Lib.Object import Object
+from Adapter import Adapter
 
-class Adapter(threading.Thread):
 
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.isEnabled = False
-        self.raw_data = []
-        self.fft_data = []
-        self.amp_data = []
-        self.p = pyaudio.PyAudio()
-        self.chunk = 1024
-        self.offset = [0]*self.chunk
-        self.stream = self.p.open(
-            format=pyaudio.paInt16,
-            channels=1,
-            rate=48000,
-            input=True,
-            output=False,
-            frames_per_buffer=self.chunk
-        )
-        self.offsetrange = []
-
-    def terminate(self):
-        self.isEnabled = False
-        self.stream.close()
-        self.p.terminate()
-
-    def run(self):
-        self.isEnabled = True
-        print("Bitte leise sein")
-        for i in range(50):
-            raw = struct.unpack(str(2*self.chunk)+ 'B', self.stream.read(self.chunk, exception_on_overflow=False))
-            print("it works")
-            ad = raw[:]
-            fft = np.fft.fft(raw)
-            print("see")
-            fft = np.abs(fft)
-            fft = list(fft[0:self.chunk])
-            fft = map(self.resize, fft)
-            self.offsetrange.append(fft)
-        for i in range(self.chunk):
-            full = 0
-            for j in range(len(self.offsetrange)):
-                full = full + self.offsetrange[j][i]
-            self.offset[i] = float(full) / len(self.offsetrange)
-        print("Kannst wieder laut sein")
-        while self.isEnabled:
-            data_raw = struct.unpack(str(2 * self.chunk) + 'B', self.stream.read(self.chunk, exception_on_overflow=False))
-            self.amp_data = data_raw[:]
-            data_fft = np.fft.fft(data_raw) # * np.hanning(len(data_raw)))
-            data_fft = np.abs(data_fft)
-            data_fft = list(data_fft[0:self.chunk])
-            data_fft = map(self.resize, data_fft)
-            diff = []
-            self.raw_data = data_fft[:]
-            zip1 = zip(data_fft, self.offset)
-            for a,b in zip1:
-                diff.append(a-b)
-            self.fft_data = diff[:]
-
-    def resize(self, num):
-        return (num*2)/self.chunk
 
 
 class FrSpectrum:
---
+++ Puslar.py
@@ -0,0 +1,83 @@
+from Lib.Object import Object
+from Lib.SubEngine import SubEngine
+from Adapter import Adapter
+import time
+
+class Pulsar(SubEngine):
+
+    def __init__(self, basefreqs=((10,15), (15,25), (25,35)), basecolors=((255,0,0), (0,255,0), (0,0,255)), middle=190, min_width=(50,25,2), max_width=(200, 120, 50), meteor_pro_width=7, deflection=(2,1,0.5)):
+        if len(basefreqs) is not len(basecolors) or len(min_width) is not len(max_width) or len(max_width) is not len(deflection) or len(deflection) is not len(basecolors):
+            print("Array sizes not matching")
+            return
+        self.adapter = Adapter()
+        self.adapter.daemon = True
+        self.build("Pulsar", 450, len(basefreqs))
+        self.objects = []
+        self.started = False
+        self.layercount = len(basecolors)
+        self.colors = basecolors
+        self.frequencies = basefreqs
+        self.max_width = max_width
+        self.min_width = min_width
+        self.meteor_line = meteor_pro_width
+        self.deflection = deflection
+        self.METEORS = []
+        for i in range(10):
+            self.METEORS.append(Meteor())
+        self.last = [0] * self.layercount
+        for i in range(self.layercount):
+            Obj = Pusle(middle, basecolors[i])
+            self.objects.append(Obj)
+            self.addObj(Obj, len(basecolors)-i)
+
+
+    def getStates(self):
+        return []
+
+    def update(self):
+        if not self.started:
+            self.adapter.start()
+            time.sleep(5)
+        fftdata = self.getFFTData()
+        for i in range(self.layercount):
+            max_data = max(fftdata[self.frequencies[i][0]:self.frequencies[i][1]])
+            # Needs tweaking
+            width = self.min_width[i] + self.deflection[i] * max_data
+            self.objects[i].update(width)
+            if not self.started:
+                self.last[i] = width
+            if width > self.max_width[i] or width > (self.last[i] * (100+self.meteor_line[i])/100):
+                #self.METEOR(i, width, width - self.last[i])
+                print("Meteor")
+
+        if not self.started:
+            self.started = True
+
+
+    def onMessage(self, topic, payload):
+        pass
+
+    #def METEOR(self, layerindex, width, speed):
+    #    for met in self.METEORS:
+    #        if met.isFree:
+    #            met.spawn(color=self.colors[i], width=width, speed=speed)
+    #            return
+    #    met = Meteor()
+    #    self.addObj(met.getObject(), self.layercount - layerindex)
+    #    self.METEORS.append(met)
+    #    met.spawn(color=self.colors[i], width=width, speed=speed)
+
+    def getFFTData(self):
+        return self.adapter.fft_data[:]
+
+
+
+class Pusle(Object):
+
+    def __init__(self, position, color):
+        self.build(True, position, [[-1, -1, -1]]*225)
+        self.color = color
+
+    def update(self, width):
+        self.content = [self.color]*width
+        # Weichzeichner
