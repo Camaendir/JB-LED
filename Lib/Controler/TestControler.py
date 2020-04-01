eef48bf9e4cee7daee8e4a75cf876a2b0e0465f5
Jks23456 <siebrecht12@gmail.com>
2020-04-01 13:55:43 +0200

    Controler: Structure Update

--- Lib/Controler/TestControler.py
+++ Lib/Controler/TestControler.py
@@ -1,10 +1,12 @@
 class TestControler:
 
     def __init__(self):
+        print("Controler:__init__")
         self.pixellength = 16
 
     def setup(self):
-        pass
+        print("Controler:SetUp")
 
     def setFrame(self, pFrame):
+        print("Controler:setFrame")
         print(pFrame)
